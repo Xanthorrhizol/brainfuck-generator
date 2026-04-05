5a001cc58e633e2fa383b81780a6247ee6b3f7fb
Xanthorrhizol <xanthorrhizol@proton.me>
2026-04-05 19:15:04 +0900

    TB: fix test code

--- src/test.rs
+++ src/test.rs
@@ -8,10 +8,17 @@ fn test() {
     let eng_encoded = std::str::from_utf8(&encode(eng)).unwrap().to_string();
     let kor_encoded = std::str::from_utf8(&encode(kor)).unwrap().to_string();
 
+    let eng_decoded = std::str::from_utf8(&decode(&eng_encoded))
+        .unwrap()
+        .to_string();
+    let kor_decoded = std::str::from_utf8(&decode(&kor_encoded))
+        .unwrap()
+        .to_string();
+
     println!("eng: {}", eng);
     println!("encode: {}", eng_encoded);
-    println!("decode: {}", decode(&eng_encoded));
+    println!("decode: {}", eng_decoded);
     println!("kor: {}", kor);
     println!("encode: {}", kor_encoded);
-    println!("decode: {}", decode(&kor_encoded));
+    println!("decode: {:?}", kor_decoded);
 }
